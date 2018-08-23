81fbb5643101c0e3c6ce3affcfc77ae94fb1e7a7
ear78 <ear78@aol.com>
2018-08-23 09:57:37 -0700

    erased example script function

--- emailScripts.php
+++ emailScripts.php
@@ -1,17 +1,5 @@
 <?php
 
-function my_scripts() {
-// Page ID, title, slug, or array of such.
-// e.g. is_page('My page title') - page title
-// e.g. is_page(2) - page id
-// e.g. is_page('my-page-title') - page slug
-// e.g. is_page( array( 'page1', 'page1')) - in this example an array of page slugs.
-// Check out the references for more on this function.
-    if( is_page( array( 'subscribe', 'home' ) ) {
-        wp_enqueue_script( 'script-name', 'path/to/example.js', array(), '1.0.0', true )
-    }
-}
-add_action( 'wp_enqueue_scripts', 'my_scripts' );
 
 
 /*
